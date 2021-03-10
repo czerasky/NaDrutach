bfdcdd028e585b9993fb57b3f9230b34ebd71eb7
czerasky <73547723+czerasky@users.noreply.github.com>
2021-03-10 14:41:21 +0100

    Create zad1.py

---
+++ zad1.py
@@ -0,0 +1,90 @@
+from math import *
+ #zad 1
+# a = 5
+# b = 15
+# str1 = "napis 1"
+# str2 = "napis 2"
+# list1 = [1,2,3]
+# list2 = ["element1","element2"]
+# krotka1 = ( "abcd", 786, 2.7, "Jan", 70.2)
+# krotka2 = ( "cezary"," ",21)
+# dict1 = {2:"to jest dwa"}
+# dict1 = {2:"to jest dwa", 3:"to jest trzy", 0:"."}
+# print(a)
+# print(b)
+# print(str1)
+# print(str2)
+# print(list1[2])
+# print(list2[0])
+# print(krotka1[0])
+# print(krotka2[2])
+# print(dict1[2])
+# print(dict1[0])
+
+ #zad2
+# a= 2
+# b= 4
+# dodawanie= a+b
+# odejmowanie= a-b
+# dzielenie= b/a
+# dzielenie_calkowite= b/a
+# mnozenie= a*b
+# potegowanie= a**3
+# reszta= 4%3
+# print(sqrt(b))
+# print(dodawanie)
+# print(odejmowanie)
+# print(dzielenie)
+# print(dzielenie_calkowite)
+# print(mnozenie)
+# print(potegowanie)
+# print(reszta)
+
+# zad3
+# a=5
+# b=2
+# c=3
+# d=2
+# e=20
+# f=10
+# a +=1
+# b -=1
+# c *=2
+# d /=2
+# e **=2
+# f %=6
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+# print(f)
+
+#zad 4
+# print(exp(10))
+
+# zad5
+# imie="CEZARY"
+# nazwisko="KOMOR"
+# print(imie.capitalize() + nazwisko.capitalize())
+
+#zad6
+# string = "Tancz z nami jak malpa Tancz z nami jak malpa ej"
+# substring = "malpa"
+# count = string.count(substring)
+# print(count)
+
+# zad7
+# slowo = "element"
+# print(slowo[1] + slowo[6])
+
+#zad 8
+# string = "Tancz z nami jak malpa Tancz z nami jak malpa ej"
+# potnij = string.split()
+# print(potnij)
+
+# zad 9
+# string = "strzelaj w opony"
+# float = 21.37
+# hexD = 128
+# print("%s %f %s" % (string, float, hex(hexD)))
