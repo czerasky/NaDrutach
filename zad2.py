12f1f1dadb13be8d35731154e0ae369c3ca76fe2
czerasky <73547723+czerasky@users.noreply.github.com>
2021-03-10 14:41:58 +0100

    zadania 2

---
+++ zad2.py
@@ -0,0 +1,57 @@
+# zad1
+# lista = ["koszykowka", "hokej", "baseball"]
+# lista.reverse()
+# lista.append("siatkowka")
+# print(lista)
+# zad2
+# slownik = {
+#     "itd": "i tak dalej",
+#     "itp": "i tym podobne",
+#     "pt.": "piatek"
+# }
+# print(slownik)
+# zad3
+# slownik2 = {
+#     "fps": ["cod","rainbow six"],
+#     "rpg": ["gothic","elder scrolls"],
+#     "moba": "dota2"
+# }
+# print(slownik2)
+# print(len(slownik2))
+# zad4
+# x = input("napisz zdanie\n")
+# xd = x.count("a")
+# xd2 = x.count("A")
+# xd3 = xd + xd2
+# print("twoje zdanie:" " " +x, "ma liter a:", + xd3)
+# zad6
+# a = input("wpisz liczbe a calkowita\n")
+# b = input("wpisz liczbe b calkowita\n")
+# c = input("wpisz liczbe c calkowita\n")
+# if a>b and a>c :
+#     print("a jest najwieksze")
+# elif b>a and b>c :
+#     print("b jest najwieksze")
+# elif c>a and c>b :
+#     print("c jest najwieksze")
+# elif a == b and a > c and b > c :
+#     print("a i b sa najwieksze")
+# elif b == c and b > a and c > a :
+#     print("b i c sa najwieksze")
+# elif a == c and a > b and c > b :
+#     print("a i c sa najwieksze")
+# else :
+#     print("wszystkie trzy sa rowne")
+# zad7
+# lista = [1,2,3,4,5.2,5.4]
+# lista_kwadratowa = [number ** 2 for number in lista]
+# print(lista_kwadratowa)
+# zad10
+import math
+# a = input("podaj liczbe\n")
+# if int(a)>0:
+#     pierwiastek = math.sqrt(int(a))
+#     print(pierwiastek)
+# else:
+#     print("nie wolno ujemnych")
+#
