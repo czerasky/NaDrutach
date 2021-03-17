30b46339e091b8ad4496a79ab815d95319363d0f
czerasky <73547723+czerasky@users.noreply.github.com>
2021-03-17 21:36:56 +0100

    Create zad3.py

---
+++ zad3.py
@@ -0,0 +1,19 @@
+# ZAd 1
+# A=[1 -x for x in  range(1, 11, 1)]
+# print(A)
+# B=[4**y for y in range(8)]
+# print(B)
+# C=[x for x in B if x % 2 == 0]
+# print(C)
+# zad2
+# from random import randint
+# lista=[randint(0, 101) for x in range(10)]
+# lista.sort()
+# print(lista)
+# parzystalista=[x for x in lista if x%2==0]
+# print(parzystalista)
+# zad3
+# prod = {"ser": "kg", "woda": "ml", "szynka": "kg","przyprawa meksykanska": "szt"}
+# print(prod)
+# kgprod = [produkt for produkt in prod.keys() if prod[produkt] =="kg"]
+# print(kgprod)
